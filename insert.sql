5d51842c8c7243792a2ca0f74a3ffca1ec4f93a9
mo1357897 <139538347+mo1357897@users.noreply.github.com>
2023-07-31 01:51:27 +0900

    commit

--- insert.sql
+++ insert.sql
@@ -1,12 +1,15 @@
 --テーブルの作成
-CREATE TABLE employee(
-	id      CHAR(5)      PRIMARY KEY,
-	name    VARCHAR(10),
-	sex     CHAR(2),
-	section VARCHAR(10)
-)
+CREATE OR REPLACE TABLE employee(
+	id            CHAR(5)      PRIMARY KEY,
+	name          VARCHAR(10),
+	sex           CHAR(2),
+	section       VARCHAR(10),
+    CRE_DATE_TIME VARCHAR(20)
+);
+
+--現在日時取得
+variable nowdatetime VARCHAR2(20); /*変数nowdatetimeを宣言*/
+execute :nowdatetime := select GETDATE(); /*現在日時を取得し、取得した値をnowdatetimeに格納*/
 
 --テーブルへデータの登録
-INSERT employee (id, name, sex, section) VALUES ('00001', '鈴木一郎', '男', '営業課')
-INSERT employee (id, name, sex, section) VALUES ('00002', '田中二郎', '男', '総務課')
-INSERT employee (id, name, sex, section) VALUES ('00003', '佐藤三郎', '男', '経理課')
+INSERT employee (id, name, sex, section,CRE_DATE_TIME) VALUES ('00001', '鈴木一郎', '男', :nowdatetime);
